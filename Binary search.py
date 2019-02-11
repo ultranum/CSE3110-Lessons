82ac7b388161af648ee2212c3981ea83e91a693f
Garrett Chen <g.chen4@share.epsb.ca>
2019-02-11 12:21:20 -0700

    binary vs lienar

--- Binary search.py
+++ Binary search.py
@@ -1,5 +1,12 @@
-numlist = [2,3,4,5,6,7,8,9,10,11,20,30,40,50,60,70,80,90,91,92,93,94,95,100,999]
-wantednum = 95
+'''
+title: Binary search
+author: garrett
+date created: 8-2-2019
+'''
+
+'''
+numlist = [2,3,4,5,6,7,8,9,10,11,20,30,40,50,60,69,70,80,90,91,92,93,94,95,100,999]
+wantednum = 80
 midpoint = len(numlist)//2
 while numlist[midpoint] != wantednum:
     if numlist[midpoint] == wantednum:
@@ -16,3 +23,25 @@ while numlist[midpoint] != wantednum:
         midpoint = len(numlist) // 2
 else:
     print('got {}'.format(numlist[midpoint]))
+'''
+
+dat = [1,3,5,7,11,13,17,19,23]
+
+def binSearch(li,val):
+    start = 0  # lowest index value to calculate midpoint
+    end = len(li)-1  # highest index value to calculate midpoint
+
+    while start <= end:
+        midp = (start + end) //2  # midpoint calc
+        print(li[midp])
+        if li[midp] == val: # found test
+            return True
+        elif val > li[midp]: # if the value is bigger than the midpoint,
+            # redefine the lowest index value to be one higher than the midpoint index value
+            start = midp + 1
+        else: # if the value is smaller than the midpoint, redefine the highest index value to be one lower than the
+            # midpoint
+            end = midp - 1
+    return False
+
+print(binSearch(dat, 1))
---
+++ binary vs linear.py
@@ -0,0 +1,41 @@
+import random, time, math
+
+li = []
+for i in range(60000):
+    if len(li) < 30000:
+        if random.randrange(2) == 1:
+            li.append(i)
+totaltimelin = []
+ttl = 0
+totaltimebin = []
+ttb = 0
+print(len(li))
+print(li)
+def linear():
+    for m in range(30):
+        num = li[random.randrange(len(li))]
+        startTime = time.perf_counter()
+        for i in range(len(li)):
+            if li[i] == num:
+                break
+        endTime = time.perf_counter()
+        totaltimelin.append(endTime - startTime)
+    print(math.mean(totaltimelin))
+
+def binary():
+    start = 0  # lowest index value to calculate midpoint
+    end = len(li) - 1  # highest index value to calculate midpoint
+
+    while start <= end:
+        midp = (start + end) // 2  # midpoint calc
+        print(li[midp])
+        if li[midp] == val:  # found test
+            return True
+        elif val > li[midp]:  # if the value is bigger than the midpoint,
+            # redefine the lowest index value to be one higher than the midpoint index value
+            start = midp + 1
+        else:  # if the value is smaller than the midpoint, redefine the highest index value to be one lower than the
+            # midpoint
+            end = midp - 1
+    return False
+linear()
