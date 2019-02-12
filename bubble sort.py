690f6676d5d91206b44ac70e2a7e40adebac89f2
Garrett Chen <g.chen4@share.epsb.ca>
2019-02-12 11:54:40 -0700

    sort

--- bubble sort.py
+++ bubble sort.py
@@ -3,6 +3,11 @@ title: bubble sort
 author: garrett
 date: 12-2-2019
 '''
-li = []
-length = len(li)
-for i in length:
+li = [2,4,6,8,10,1,3,5,7,9]
+length = 10
+store = 0
+for i in range(10):
+    if li[i+1] < li[i]:
+        store = li[i+1]
+        li[i].append(store)
+        print(li)
